65dda862ad4b807b02e3d1b8f0cc34ded6b4d242
Angela <Angela.ohh@gmail.com>
2019-02-20 13:54:39 -0800

    blackjack_score method

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,30 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "King", "Queen", "Jack"]
 
 def blackjack_score(hand)
 
+  #   num_of_cards = rand(2..5)
+  #   hand = []
+  #   num_of_cards.times do
+  #     random_index = rand(0..VALID_CARDS.length - 1)
+  #     hand << VALID_CARDS[random_index]
+  #   end
+
+  hand.map! do |card|
+    if card == "King" || card == "Queen" || card == "Jack"
+      card = 10
+    else
+      card
+    end
+  end
+
+  score = hand.sum
+
+  #lets Ace equal 11 if the 1 value total is less than 11
+  if hand.include? 1 && score <= 11
+    score = score + 10
+  end
+
+  return score
 end
