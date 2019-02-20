de71992fc69829cfe271e30176a4e579fb704b53
Angela <Angela.ohh@gmail.com>
2019-02-20 14:10:40 -0800

    Fixed ace testing

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -3,14 +3,6 @@
 VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "King", "Queen", "Jack"]
 
 def blackjack_score(hand)
-
-  #   num_of_cards = rand(2..5)
-  #   hand = []
-  #   num_of_cards.times do
-  #     random_index = rand(0..VALID_CARDS.length - 1)
-  #     hand << VALID_CARDS[random_index]
-  #   end
-
   hand.map! do |card|
     if card == "King" || card == "Queen" || card == "Jack"
       card = 10
@@ -20,11 +12,17 @@ def blackjack_score(hand)
   end
 
   score = hand.sum
+  puts score
 
   #lets Ace equal 11 if the 1 value total is less than 11
-  if hand.include? 1 && score <= 11
-    score = score + 10
+  if hand.include? 1
+    if score < 12
+      puts "this is true"
+      score += 10
+    end
   end
 
   return score
 end
+
+blackjack_score([1, 3, 6])
